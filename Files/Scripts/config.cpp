d6cdfc477c62d687e46196ebc3ad6d9b1ea42d59
Krzysztof Szypuła <kszypula22@gmail.com>
2024-04-10 22:14:23 +0200

    DIFR flag, new gitignore, change to folder structure
    Added:
     - DIFR FLAG (Standard)
    
    Updated:
     - Added PAA to list of ignored files in gitignore file to prevent asset leaking
    
    Project Changes:
     - Textures now reside in data directory as its standard way of doing it for day unlike custom /scripts/assets path.

--- Files/Scripts/config.cpp
+++ Files/Scripts/config.cpp
@@ -4,7 +4,7 @@ class CfgPatches
     {
         requiredAddons[] =
         {
-            // ""
+            "DZ_Data",
         };
     };
 };
@@ -14,7 +14,18 @@ class cfgVehicles
     class PersonalRadio;
     class resrp_Radio: PersonalRadio
         {
-            displayName="Boosted Personal Radio";
+            displayName="Survivalist Radio";
+            descriptionShort="A clearly modified personal radio. Smells oddly like doritos, cheese, and MREs, which is probably more concerning. The antena appears just a little bit longer and thicker, most likley to accomodate stronger signal.";
             range=10000;
-        };  
+        };
+        
+    class Flag_Base;
+	class Flag_Difr: Flag_Base
+	{
+		scope = 2;
+        displayName="Deer Isle First Responders flag";
+        descriptionShort="Flag seemingly of few First Responders joining together after apocalypse. At this point who knows where their adventures led them to....";
+		hiddenSelectionsTextures[] = {"ResurgenceRP_Dev\data\flag_difr.paa"};
+        color="Difr";
+	}; 
 };
